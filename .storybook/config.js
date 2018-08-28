3e5d140fab8d796fda157fbc1462678c64357284
Przemek Krupiński <przemek.krupinski@joist.com>
2018-08-28 14:40:17 +0200

    Installing and configuring Storybook config plugins

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,16 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options'
+import appPackage from '../package.json'
 
 function loadStories() {
   require('../src/stories');
 }
+
+setOptions({
+  name: `My Storybook v${appPackage.version}`,
+  url: '/storybook/index.html',
+  addonPanelInRight: true,
+  hierarchySeparator: /\//
+})
 
 configure(loadStories, module);
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Button from '../components/Button';
+import { boolean, text, select, withKnobs } from '@storybook/addon-knobs/react'
+import Button, { COLORS } from '../components/Button';
 
 storiesOf('Buttons', module)
-  .add('Button', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .addDecorator(withKnobs)
+  .add('Button',
+    () => (<Button disabled={boolean('Disabled', false)}
+      color={select('Color', Object.values(COLORS))}
+      isLoading={boolean('Is loading', false)}
+      fillWidth={boolean('Fill width', false)}
+      roundish={boolean('Roundish', false)}
+      noFill={boolean('No fill', false)}
+      noOutline={boolean('No outline', false)}
+      onClick={action('clicked')}>{text('Label', 'Button')}</Button>)
+  )
