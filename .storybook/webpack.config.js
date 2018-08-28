abb2a8b84c6d0a55d9e46ea69c5cade1a3586432
Przemek Krupiński <przemek.krupinski@joist.com>
2018-08-28 14:14:54 +0200

    Tweaked storybook webpack config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        // "oneOf" will traverse all following loaders until one will
+        // match the requirements. When no loader matches it will fall
+        // back to the "file" loader at the end of the loader list.
+        oneOf: [
+          {
+            test: /\.(css|scss)$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  modules: true,
+                  sourceMap: true,
+                  minimize: true,
+                  localIdentName: '[local]___[hash:base64:5]'
+                },
+              },
+              require.resolve('sass-loader'),
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    require('autoprefixer')({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              }
+            ]
+          },
+          {
+            test: /\.svg$/,
+            use: [
+              {
+                loader: "babel-loader"
+              },
+              {
+                loader: "react-svg-loader",
+                options: {
+                  jsx: false // true outputs JSX tags
+                }
+              }
+            ]
+          },
+          // This loader is used since importing .md files via ES6 modules turn out
+          // in default markdown parse and this causes some problems when rendering
+          // it again in storybook info. This has to remain this way till
+          // storybook releases storybook-info v4.0.0
+          {
+            test: /\.md.storybook$/,
+            use: 'raw-loader',
+          },
+          {
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.ejs$/, /\.json$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          }
+        ]
+      }
+    ]
+  }
+}
