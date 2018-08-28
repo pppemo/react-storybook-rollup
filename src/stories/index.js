6891ca724cfb966ddbb973835078bc883517f8b3
Przemek Krupiński <przemek.krupinski@joist.com>
2018-08-28 14:59:03 +0200

    Configured "withInfo" plugin

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { boolean, text, select, withKnobs } from '@storybook/addon-knobs/react'
+import { withInfo } from '@storybook/addon-info'
+
 import Button, { COLORS } from '../components/Button';
+import ButtonREADME from '../components/Button/README.md.storybook'
 
 storiesOf('Buttons', module)
   .addDecorator(withKnobs)
-  .add('Button',
+  .add('Button', withInfo(ButtonREADME)(
     () => (<Button disabled={boolean('Disabled', false)}
       color={select('Color', Object.values(COLORS))}
       isLoading={boolean('Is loading', false)}
@@ -15,5 +18,5 @@ storiesOf('Buttons', module)
       roundish={boolean('Roundish', false)}
       noFill={boolean('No fill', false)}
       noOutline={boolean('No outline', false)}
-      onClick={action('clicked')}>{text('Label', 'Button')}</Button>)
+      onClick={action('clicked')}>{text('Label', 'Button')}</Button>))
   )
